6c5762ce96c47dc91fb73313da444adf1e95d8c7
Edge <edge27452696803@gmail.com>
2024-11-08 01:10:19 +0000

    lab 01 - 02

---
+++ lab01/Expression.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 100
+
+typedef struct {
+    int data[MAX_LEN];
+    int top;
+} Stack;
+
+void initStack(Stack *s) {
+    s->top = -1;
+}
+
+Stack getStack() {
+    Stack s;
+    initStack(&s);
+    return s;
+}
+
+int isEmpty(Stack *s) {
+    return s->top == -1;
+}
+
+void push(Stack *s, int value) {
+    s->top++;
+    s->data[s->top] = value;
+}
+
+int pop(Stack *s) {
+    if (isEmpty(s)) {
+        printf("Stack is empty\n");
+        return -1;
+    }
+    int value = s->data[s->top];
+    s->top--;
+    return value;
+}
+
+int get_expression(char* expression) {
+    Stack s = getStack();
+    for (unsigned int i = 0; i < strlen(expression) - 1; i++) {
+        int n = strlen(expression);
+        if (expression[i] == ' ') {
+            continue;
+        }
+        char ch = expression[i];
+        if (ch == '(') {
+            push(&s, ch);
+        } else if (ch == ')') {
+            int value = 0;
+            while (s.data[s.top] != '(') {
+                if (s.data[s.top] == '+') {
+                    value += pop(&s);
+                } else if (s.data[s.top] == '-') {
+                    value -= pop(&s);
+                } else {
+                    value = pop(&s);
+                }
+            }
+            push(&s, value);
+        } else if (ch == '+' || ch == '-') {
+            push(&s, ch);
+        } else if (ch == '*' || ch == '/') {
+            int value = pop(&s);
+            int value2 = pop(&s);
+            if (ch == '*') {
+                push(&s, value * value2);
+            } else {
+                push(&s, value / value2);
+            }
+        } else {
+            int value = ch - '0';
+            push(&s, value);
+        }
+    }
+    int value = pop(&s);
+    while (!isEmpty(&s)) {
+        char ch = pop(&s);
+        if (ch == '+') {
+            value += pop(&s);
+        } else {
+            value -= pop(&s);
+        }
+    }
+    return value;
+}
+
+int main() {
+    char expression[MAX_LEN];
+    printf("Enter expression: ");
+    fgets(expression, MAX_LEN, stdin);
+    int result = get_expression(expression);
+    printf("Result: %d\n", result);
+    return 0;
+}
---
+++ lab01/Ploy.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int coeff;  // 系数
+    int exp;    // 指数
+    struct Node *next;  // 指向下一个节点的指针
+} Node;
+
+Node *create(int coeff, int exp) {
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->coeff = coeff;
+    p->exp = exp;
+    p->next = NULL;
+    return p;
+}
+
+void print(Node *head) {
+    Node *p = head;
+    while (p) {
+        printf("%dx^%d ", p->coeff, p->exp);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+void addNode(Node **head, Node *node) {
+    if (*head == NULL) {
+        *head = node;
+    } else {
+        Node *p = *head;
+        while (p->next) {
+            p = p->next;
+        }
+        p->next = node;
+    }
+}
+
+Node* addPloy (Node *poly1, Node *poly2) {
+    Node *result = NULL;
+    Node *p1 = poly1;
+    Node *p2 = poly2;
+    while (p1 != NULL && p2 != NULL) {
+        if (p1->exp > p2->exp) {
+            addNode(&result, p1);
+            p1 = p1->next;
+        } else if (p1->exp < p2->exp) {
+            addNode(&result, p2);
+            p2 = p2->next;
+        } else {
+            if (p1->coeff + p2->coeff == 0) {
+                p1 = p1->next;
+                p2 = p2->next;
+            } else {
+                addNode(&result, create(p1->coeff + p2->coeff, p1->exp));
+                p1 = p1->next;
+                p2 = p2->next;
+            }
+        }
+    }
+    return result;
+}
+
+
+int main() {
+    Node *poly1 = NULL;
+    Node *poly2 = NULL;
+    addNode(&poly1, create(1, 2));
+    addNode(&poly1, create(2, 1));
+    addNode(&poly1, create(3, 0));
+    addNode(&poly2, create(2, 2));
+    addNode(&poly2, create(3, 1));
+    addNode(&poly2, create(4, 0));
+    Node *result = addPloy(poly1, poly2);
+    print(result);
+    return 0;
+}
